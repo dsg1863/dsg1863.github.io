129d1f9c8c5d678db9fb3a890e4ce3b2f1312373
DSG1863 <lularocha@protonmail.com>
2024-10-17 18:39:06 -0300

    navbar

--- navbar/navbar-2024-2.js
+++ navbar/navbar-2024-2.js
@@ -1,4 +1,4 @@
-// JavaScript to add a "Voltar para índice" link to every page.
+// JavaScript to add a "Voltar para índice" link to every page
 window.onload = function() {
     // Create the link element
     var navBar = document.createElement('div');
@@ -6,7 +6,7 @@ window.onload = function() {
     
     // Set the link attributes
     link.href = 'https://dsg1863.github.io/2024-2/';
-    link.textContent = '< Projetos 2024-2';
+    link.textContent = 'Projetos 2024-2';
     
     // Apply the CSS class
     navBar.classList.add('nav-bar');
