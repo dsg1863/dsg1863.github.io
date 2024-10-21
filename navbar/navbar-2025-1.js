47e811efeba0b5a365803375cde036774260d271
DSG1863 <lularocha@protonmail.com>
2024-10-21 12:46:56 -0300

    atualiza js navbar

--- navbar/navbar-2025-1.js
+++ navbar/navbar-2025-1.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var link = document.createElement('a');
     
     // Set the link attributes
-    link.href = 'https://dsg1863.github.io/2025-1/';
+    link.href = '/2025-1/';
     link.textContent = 'Projetos 2025-1';
     
     // Apply the CSS class
@@ -17,4 +17,3 @@ window.onload = function() {
     // Add the nav bar to the body of every page
     document.body.insertBefore(navBar, document.body.firstChild);
 };
-
