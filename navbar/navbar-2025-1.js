18b58d4b93e6ac2a2187ade547357c733e6bd576
DSG1863 <lularocha@protonmail.com>
2025-02-20 08:02:38 -0300

    Update navbar-2025-1.js

--- navbar/navbar-2025-1.js
+++ navbar/navbar-2025-1.js
@@ -1,19 +1,21 @@
-// JavaScript to add a "Voltar para índice" link to every page
 window.onload = function() {
-    // Create the link element
-    var navBar = document.createElement('div');
-    var link = document.createElement('a');
-    
-    // Set the link attributes
-    link.href = '/2025-1/';
-    link.textContent = 'Projetos 2025-1';
-    
-    // Apply the CSS class
-    navBar.classList.add('nav-bar');
-    
-    // Append the link to the nav bar
-    navBar.appendChild(link);
-    
-    // Add the nav bar to the body of every page
-    document.body.insertBefore(navBar, document.body.firstChild);
+    // Check if body has the correct class
+    if (document.body.classList.contains('semestre-2025-1')) {
+        // Create the link element
+        var navBar = document.createElement('div');
+        var link = document.createElement('a');
+        
+        // Set the link attributes
+        link.href = '/2025-1/';
+        link.textContent = 'Projetos 2025-1';
+        
+        // Apply the CSS class
+        navBar.classList.add('nav-bar');
+        
+        // Append the link to the nav bar
+        navBar.appendChild(link);
+        
+        // Add the nav bar to the body of every page
+        document.body.insertBefore(navBar, document.body.firstChild);
+    }
 };
